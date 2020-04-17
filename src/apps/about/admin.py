bf96c44ec2e78f15e020fa780ad8c355ef32bcad
Katsiaryna Kutseika <katslpk@gmail.com>
2020-04-17 21:42:18 +0300

    add models

--- src/apps/about/admin.py
+++ src/apps/about/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 
-# Register your models here.
+from apps.about.models import UserInfo
+
+
+@admin.register(UserInfo)
+class UserInfoAdminModel(ModelAdmin):
+    pass
--- src/apps/about/models.py
+++ src/apps/about/models.py
@@ -1,3 +1,12 @@
-from django.db import models
+from django.db import models as m
 
-# Create your models here.
+class UserInfo(m.Model):                    #созд база с 2мя колонками
+    name = m.TextField(unique=True)         #нельзя пустую колонку, и будет еще уникальна
+    greeting = m.TextField(null=True, blank=True)       #тип чтобы были доступны пустые стоки
+
+    class Meta:
+        verbose_name_plural = "User Info"
+
+    def __str__(self):
+        return f"UserInfo(id={self.pk},name={self.name!r})"
+        
