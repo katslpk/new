85f90893560ef1b7f7ca060bd6d874d2aa2d575c
Katsiaryna Kutseika <katslpk@gmail.com>
2020-06-04 23:09:03 +0300

    add api

---
+++ src/apps/api/impl/v1/views.py
@@ -0,0 +1,14 @@
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
+
+from apps.api.impl.v1.serializers import WorkSerializer
+from apps.education.models import Work
+
+
+class WorkViewSet(ModelViewSet):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = WorkSerializer
+
+
+    def get_queryset(self):
+        return Work.objects.all()
