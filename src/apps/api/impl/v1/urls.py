c9f4aabbd478b5fd893d05a378ddead7d03ef9dc
katslpk <62587219+katslpk@users.noreply.github.com>
2020-06-09 20:10:39 +0300

    serverless + tg (#9)

--- src/apps/api/impl/v1/urls.py
+++ src/apps/api/impl/v1/urls.py
@@ -1,7 +1,9 @@
 from django.urls import include
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework.routers import DefaultRouter
 
+from apps.api.impl.v1.views import TelegramView
 from apps.api.impl.v1.views import WorkViewSet
 
 router = DefaultRouter()
@@ -9,4 +11,5 @@ router.register("work", WorkViewSet, "work")
 
 urlpatterns = [
     path("", include(router.urls)),
+    path("tg/", csrf_exempt(TelegramView.as_view())),
 ]
--- src/apps/api/impl/v1/views.py
+++ src/apps/api/impl/v1/views.py
@@ -1,3 +1,9 @@
+import json
+
+import requests
+from django.http import HttpResponse
+from django.views import View
+from dynaconf import settings
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
@@ -11,3 +17,21 @@ class WorkViewSet(ModelViewSet):
 
     def get_queryset(self):
         return Work.objects.all()
+
+
+class TelegramView(View):
+    def post(self, *args, **kwargs):
+        try:
+            payload = json.loads(self.request.body)
+            message = payload["message"]
+            text = message["text"]
+            chat_id = message["chat"]["id"]
+
+            r = requests.post(
+                f"https://api.telegram.org/bot{settings.TG}/sendMessage",
+                json={"chat_id": chat_id, "text": text.upper()},
+            )
+            print(f"XXX sendMessage resp: {r}")
+        except Exception as err:
+            print(err)
+        return HttpResponse()
