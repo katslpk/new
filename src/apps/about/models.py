81426bb4a4e97e8e6f2ffa650af5b3749f7800f1
Katsiaryna Kutseika <katslpk@gmail.com>
2020-06-11 21:41:47 +0300

    make format

--- src/apps/about/models.py
+++ src/apps/about/models.py
@@ -3,13 +3,15 @@ from django.db import models
 
 User = get_user_model()
 
+
 class CarInfo(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     car_model = models.TextField(null=True, blank=True)
     car_year = models.DateField(null=True, blank=True)
-    engine_volume = models.DecimalField(max_digits=2, decimal_places=1, null=True, blank=True)
+    engine_volume = models.DecimalField(
+        max_digits=2, decimal_places=1, null=True, blank=True
+    )
     engine_type = models.TextField(null=True, blank=True)
     vin = models.TextField(max_length=17, unique=True, primary_key=True)
     car_license_plate = models.TextField(max_length=7, null=True, blank=True)
     additional_info = models.TextField(null=True, blank=True)
-
--- src/apps/about/views.py
+++ src/apps/about/views.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-
-from django.views.generic import TemplateView, ListView
+from django.views.generic import ListView
+from django.views.generic import TemplateView
 
 from apps.about.models import CarInfo
 from apps.onboarding.models import Profile
+
 
 class IndexView(LoginRequiredMixin, TemplateView):
     template_name = "about/index.html"
@@ -12,11 +13,10 @@ class IndexView(LoginRequiredMixin, TemplateView):
         context = super(IndexView, self).get_context_data(**kwargs)
         user = self.request.user
         profile = Profile.objects.get(user_id=user.id)
-        context['name'] = profile.name
+        context["name"] = profile.name
         return context
 
 
 class CarInfoView(LoginRequiredMixin, ListView):
     template_name = "about/index.html"
     model = CarInfo
-
