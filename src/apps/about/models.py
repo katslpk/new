394c1140b4e06f9b57a25d23ae0c7b78a10e5616
Katsiaryna Kutseika <katslpk@gmail.com>
2020-08-30 10:48:45 +0300

    fix model CarInfo

--- src/apps/about/models.py
+++ src/apps/about/models.py
@@ -8,13 +8,13 @@ User = get_user_model()
 class CarInfo(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     car_model = models.TextField(null=True, blank=True)
-    car_year = models.DateField(null=True, blank=True)
+    car_year = models.DecimalField (null=True, blank=True, max_digits=4, decimal_places=0)
     engine_volume = models.DecimalField(
         max_digits=2, decimal_places=1, null=True, blank=True
     )
     engine_type = models.TextField(null=True, blank=True)
     vin = models.TextField(max_length=17, null=True, blank=True)
-    car_license_plate = models.TextField(max_length=7, null=True, blank=True)
+    car_license_plate = models.TextField(null=True, blank=True)
     additional_info = models.TextField(null=True, blank=True)
 
     def get_absolute_url(self):
