1a30149c45eb83195ed775ab63bd74acbbc132f8
Katsiaryna Kutseika <katslpk@gmail.com>
2020-09-13 14:17:07 +0300

    Revert "Add stations, types of work, delete work"
    This reverts commit 57ca52713df368ae1cd2d9eef0b1cd31ee81c075.

--- src/apps/service/forms/station_add.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm
-
-from apps.service.models import Station
-
-
-class StationAddForm(ModelForm):
-    class Meta:
-        model = Station
-        fields = "__all__"
--- src/apps/service/forms/type_add.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm
-
-from apps.service.models import Type
-
-
-class TypeAddForm(ModelForm):
-    class Meta:
-        model = Type
-        fields = "__all__"
